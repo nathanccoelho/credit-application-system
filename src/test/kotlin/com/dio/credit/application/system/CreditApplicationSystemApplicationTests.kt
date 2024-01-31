457e36aba39416477fa74c8ef5be3886c11b4020
nathanccoelho <nathancoelho.job@gmail.com>
2024-01-30 23:45:18 -0300

    first commit

---
+++ src/test/kotlin/com/dio/credit/application/system/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dio.credit.application.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
