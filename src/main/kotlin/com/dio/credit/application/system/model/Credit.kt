8945beb2e83b132a88dc86766fc570df2a7fdeca
nathanccoelho <nathancoelho.job@gmail.com>
2024-02-01 10:01:45 -0300

    Config JPA and hibernte

--- src/main/kotlin/com/dio/credit/application/system/model/Credit.kt
+++ src/main/kotlin/com/dio/credit/application/system/model/Credit.kt
@@ -10,7 +10,7 @@ data class Credit(
     @Column (nullable = false, unique = true ) val creditCode: UUID = UUID.randomUUID(),
     @Column (nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
     @Column (nullable = false) val dayFirstInstallment: LocalDate,
-    @Column (nullable = false) git val numberOfInstallment: Int = 0,
+    @Column (nullable = false) val numberOfInstallment: Int = 0,
     @Enumerated val status: Status = Status.IN_PROGRESS,
     @ManyToOne var customer: Customer? = null,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
