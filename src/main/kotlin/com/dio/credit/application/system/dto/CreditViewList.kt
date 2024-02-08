f4b5e858ce0e9ff933482b0d087c86c712926673
nathanccoelho <nathancoelho.job@gmail.com>
2024-02-08 17:04:18 -0300

    Testing ok, falta colocar exceções e responsesEntity quando fizererm errado

--- src/main/kotlin/com/dio/credit/application/system/dto/CreditViewList.kt
+++ src/main/kotlin/com/dio/credit/application/system/dto/CreditViewList.kt
@@ -2,17 +2,20 @@ package com.dio.credit.application.system.dto
 
 import com.dio.credit.application.system.model.Credit
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.util.UUID
 
 data class CreditViewList(
     val creditCode:UUID,
     val creditValue: BigDecimal,
-    val numberOfInstallment: Int
+    val numberOfInstallment: Int,
+    val dayFirstInstallment: LocalDate
 
 ){
     constructor(credit: Credit ): this(
         creditCode = credit.creditCode,
         creditValue = credit.creditValue,
-        numberOfInstallment = credit.numberOfInstallment
+        numberOfInstallment = credit.numberOfInstallment,
+        dayFirstInstallment = credit.dayFirstInstallment
     )
 }
